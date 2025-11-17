b4baa9dd446eefebe81c218a2cc9fd42977a92d8
certainlyPeterK <peterkolts@mail.ru>
2025-11-17 13:40:05 +0300

    Created Task 1.3.py

---
+++ Task 1.3.py
@@ -0,0 +1,27 @@
+num_1 = 7
+num_2 = 10
+num_3 = 4
+summ = num_1 + num_2 + num_3
+print("Сумма всех целых чисел:", summ)
+num_1 = 7.9
+num_2 = 21.3
+num_3 = float(num_3)
+summ = num_1 + num_2 + num_3
+print("Сумма всех нецелых чисел:", summ)
+num_1 = 130
+num_2 = 4
+num_3 = 2
+multiplying = num_1 * num_2 * num_3
+print("Умножение всех чисел:", multiplying)
+division = num_1/num_2/num_3
+print("Деление:", division)
+num_1 = 2
+num_2 = 3
+num_3 = 4
+degree = num_1 ** num_2 ** num_3
+print("Числа в степени:", degree)
+num_1 = 2
+num_2 = 8
+num_3 = 5
+math = ((43 + num_1) + (num_2 + 67) - (num_3 * 2) // 2)
+print(math)
