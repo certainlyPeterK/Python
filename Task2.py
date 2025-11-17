b0c5b9915763dfb6cde74dab1b895bb8ea49497c
certainlyPeterK <peterkolts@mail.ru>
2025-11-17 13:30:37 +0300

    Created Task2.py

---
+++ Task2.py
@@ -0,0 +1,10 @@
+path = "C:\\Users\\MainAdmin\\Desktop\\programs"
+print(path)
+path += "\\game.exe"
+print(path)
+path = "C:\\Users\\MainAdmin\\Desktop\\files"
+path += "\\picture.png"
+print(path)
+path = "C:\\Games\\city simulator"
+path *= 2
+print("Error path:", path)
