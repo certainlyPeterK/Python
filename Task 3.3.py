a99bead2304c8ebd660502bf5cc0e8c6c959f9ed
certainlyPeterK <peterkolts@mail.ru>
2025-11-17 14:07:40 +0300

    Create Task 3.3.py

---
+++ Task 3.3.py
@@ -0,0 +1,6 @@
+import math
+a = int(input("AAAAAAAAAAAAAAAAAA"))
+b = int(input("BBBBBBBBBBBBBBBBBB"))
+c = int(input("CCCCCCCCCCCCCCCCCCCC"))
+p = (a + b + c)/2.0
+print(math.sqrt(p * (p-a) * (p-b) * (p-c)))
