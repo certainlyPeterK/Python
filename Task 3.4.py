ae9f26a3b3b7dab917f3fac270376642c9e63c03
certainlyPeterK <peterkolts@mail.ru>
2025-11-17 14:15:06 +0300

    Updated Task 3.4.py

--- Task 3.4.py
+++ Task 3.4.py
@@ -1,1 +1,15 @@
-
+import math
+type = int(input("Тип какой? 1 - треугольник, 2 - прямоугольник, 3 - круг:\n"))
+if type == 1:
+    a = int(input("Стороны какие? Давай ашку "))
+    b = int(input("Давай бешку "))
+    c = int(input("Давай цешку "))
+    p = (a + b + c)/2.0
+    print(math.sqrt(p * (p-a) * (p-b) * (p-c)))
+elif type == 2:
+    a = int(input("Стороны какие? Давай ашку "))
+    b = int(input("Давай бешку "))
+    print(a * b)
+else:
+    r = int(input("ААААААААААААААААААААААААА, давай радиус"))
+    print("Приближённо", 3.14 * (r**2))
