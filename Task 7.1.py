83ffd98845b551743597e0959a1ca3a72a42cb0b
certainlyPeterK <peterkolts@mail.ru>
2025-11-21 12:50:31 +0300

    Created Task 7.1.py

---
+++ Task 7.1.py
@@ -0,0 +1,7 @@
+table = [["folder", "coursework.doc", "folder", "pict.png", "data.accdb"],
+        ["icon.ico", "script.js", "index.html", "style.css", "prog.py"],
+        ["my_song.mp3", "anapa-2003.jpg", "cs_1.6.exe", "folder", "cheat.txt"],
+        ["notext.txt", "main.py", "work.pdf", "cartoon.mp4", "array.py"],
+        ["project.psd", "cycle.py", "folder", "cycle.js", "turtle.py"]]
+
+print(table)
