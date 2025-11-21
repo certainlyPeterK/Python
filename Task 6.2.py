36b7787ef991097679a5be46c422fa2876021638
certainlyPeterK <peterkolts@mail.ru>
2025-11-21 12:13:33 +0300

    Created Task 6.2.py

---
+++ Task 6.2.py
@@ -0,0 +1,16 @@
+matrix_1 = [[2, 4, 3, 6],
+           [5, 7, 1, 5]]
+
+matrix_2 = [[2, 9, 0, 2],
+           [3, 4, 7, 6]]
+
+answer_matrix = [[0, 0, 0, 0],
+                [0, 0, 0, 0]]
+
+for i in range(len(matrix_1)):
+    for j in range(len(matrix_1[i])):
+        answer_matrix[i][j] = matrix_1[i][j] * matrix_2[i][j]
+print(answer_matrix)
+
+for row in answer_matrix:
+    print(f"{row} сумма строки: {sum(row)}")
