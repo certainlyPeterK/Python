e14a8663d3b011d555284d546bfa4651d8daa13b
certainlyPeterK <peterkolts@mail.ru>
2025-11-28 12:25:43 +0300

    Created Task 4.3.2.py

---
+++ Task 4.3.2.py
@@ -0,0 +1,11 @@
+def task_432():
+    n = input()
+    if n[-4:] == ".doc":
+        print("Word file")
+    elif n[-3:] == ".py":
+        print("Python file")
+    elif n[-4:] == ".txt":
+        print("Text file")
+    else:
+        print("ААААААААААААААААААААААААААААААААААААААААААААААААААААААААА!!!!!!!!!!!")
+    
