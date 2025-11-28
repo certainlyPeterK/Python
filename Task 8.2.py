0f1fb2cb144b6cc11f7397feed35a46c76201ed9
certainlyPeterK <peterkolts@mail.ru>
2025-11-28 11:38:44 +0300

    Created Task 8.2.py

---
+++ Task 8.2.py
@@ -0,0 +1,8 @@
+sl = {'</>':13, 'script':1, '__init__':10, 'self':5, 'number_9':6, '#comment':100}
+print(f">>> {sl}")
+
+newKey = input(">>> key: ")
+newVal = input(">>> value: ")
+sl[newKey] = newVal
+
+print(sl)
