b398170b2c104396a1f84ed58b8eea7b7280f28d
certainlyPeterK <peterkolts@mail.ru>
2025-11-28 11:42:09 +0300

    Created Task 8.3.py

---
+++ Task 8.3.py
@@ -0,0 +1,7 @@
+dictionary = {}
+
+while len(dictionary) != 3:
+    newKey = input("КЛЮЮЮЮЮЮЮЮЮЧЧЧЧ: ")
+    newVal = input("ЗНАААААААААЧЧЧЧ: ")
+    dictionary[newKey] = newVal
+    print(dictionary)
