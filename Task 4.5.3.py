5fcc1f3eb0d2849b9554ac1fc0bb306fe96eec0d
certainlyPeterK <peterkolts@mail.ru>
2025-12-01 13:09:58 +0300

    Updated Task 4.5.3.py

--- Task 4.5.3.py
+++ Task 4.5.3.py
@@ -1,7 +1,12 @@
 def task_453():
     a = int(input("\n"))
     b = int(input("\n"))
+    diff = abs(a - b)
     if (a > b):
-        print("")
-
-#Недодел
+        print(f"A длиннее на {diff}")
+    elif (b > a):
+        print(f"B длиннее на {diff}")
+    else:
+        print("Они равны")
+    
+task_453()
