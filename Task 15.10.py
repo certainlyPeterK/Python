8bf772cc03e0dedff6e1ccbf2a0cfd2a79184243
certainlyPeterK <peterkolts@mail.ru>
2025-12-08 14:38:59 +0300

    Created Task 15.10.py

---
+++ Task 15.10.py
@@ -0,0 +1,25 @@
+import time
+from turtle import *
+
+bgpic('back2.png')
+color("white")
+speed(1)
+
+forward(55)
+left(90)
+circle(55, 270)
+right(90)
+forward(115)
+right(90)
+circle(-152.5, 180)
+forward(195)
+right(90)
+forward(200)
+right(57)
+forward(190)
+right(115)
+forward(80)
+right(70)
+forward(85)
+
+time.sleep(1)
