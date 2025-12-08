a65c1e9ce25ada062334feb20ab61ae28bd79f2a
certainlyPeterK <peterkolts@mail.ru>
2025-12-08 13:40:16 +0300

    Created Task 15.4.py

---
+++ Task 15.4.py
@@ -0,0 +1,16 @@
+import time
+from turtle import *
+
+speed(1)
+
+bgcolor("black")
+pencolor("white")
+
+#Ну типа эллипс
+circle(90, 90)
+circle(180, 90)
+circle(90, 90)
+circle(180, 90)
+
+time.sleep(1)
+        
